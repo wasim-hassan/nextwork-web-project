300bab38fce77041c278c376d90fc7a9869c2ce1
EC2 Default User <ec2-user@ip-172-31-0-155.ap-south-1.compute.internal>
2025-10-23 11:35:31 +0000

    Intentionally adding error to demonstrate rollback

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-isExistApp="$(pgrep httpd)"
-if [[ -n $isExistApp ]]; then
-sudo systemctl stop httpd.service
-fi
-isExistApp="$(pgrep tomcat)"
-if [[ -n $isExistApp ]]; then
-sudo systemctl stop tomcat.service
-fi
+# Add this at the top of your script to make it exit immediately if any command fails
+set -e
+
+# Your intentional error
+sudo systemctll stop httpd.service  # Misspelled command will now cause script to exit with non-zero status
+
+# To be extra sure, we can also add an explicit exit code
+exit 1
